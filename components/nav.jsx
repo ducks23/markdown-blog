998b9d2585eb274588591399525a5895caa41619
Jesse Leonard <jesse1517@gmail.com>
2023-12-21 15:53:01 -0600

    pre tailwind install

---
+++ components/nav.jsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+	return (
+		<nav>
+			<div className="nav-div-container">
+				<div>blog logo</div>
+				<div>
+					<a href="https://jesseleonard.app"> My main website</a>
+				</div>
+			</div>
+		</nav>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -4,39 +4,44 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
+import Nav from "../components/nav";
 
 export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hello this is my Markdown blog :)</p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  );
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section className={utilStyles.headingMd}>
+				<p className="paragraph_idx">
+					Welcome to my blog, I hope you like memes.{" "}
+				</p>
+			</section>
+			<section
+				className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
+			>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							<Link href={`/posts/${id}`}>{title}</Link>
+							<br />
+							<small className={utilStyles.lightText}>
+								<Date dateString={date} />
+							</small>
+						</li>
+					))}
+				</ul>
+			</section>
+		</Layout>
+	);
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
 }
