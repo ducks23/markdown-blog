ee907fce0713cadef66a8de79945b3fe3fc96681
Jesse Leonard <jesse1517@gmail.com>
2023-12-31 23:16:04 -0600

    updated mining blog

--- pages/index.js
+++ pages/index.js
@@ -20,13 +20,15 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link style={{ textAlign: "center" }} href={`/posts/${id}`}>
-                {title}
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
+              <div>
+                <Link style={{ textAlign: "center" }} href={`/posts/${id}`}>
+                  {title}
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </div>
             </li>
           ))}
         </ul>
