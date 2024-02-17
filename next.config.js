760e50795b131075f5f3e71679410df26b56041e
Jesse Leonard <jesse1517@gmail.com>
2024-02-17 09:26:46 -0600

    updated secret access key and other things

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/** @type {import(next).NextConfig} */
 const nextConfig = {
   async headers() {
     return [
@@ -16,6 +16,7 @@ const nextConfig = {
   },
   env: {
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
   },
 };
 
